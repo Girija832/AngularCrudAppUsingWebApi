67e45627fc0c9fcfb06fa2b3418ea3c7e41575a3
unknown <debajyotimallick1993@gmail.com>
2020-05-09 08:38:47 +0530

    Update appRouting file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,19 @@
-<h2 mat-dialog-title>Add New Customer</h2>
-<mat-dialog-content>
-    <div>
-        <form class="add-form"  #formControl="ngForm">
-            <label for="name">Name</label>
-            <input type="text" id="name" [(ngModel)]="data.name" class="form-control" name="name">
-            <label for="email">Email</label>
-            <input type="text" id="email" [(ngModel)]="data.email" class="form-control" name="email">
-            <label for="address">Address</label>
-            <input type="text" id="address" [(ngModel)]="data.address" class="form-control" name="address">
-            <label for="phone">Phone</label>
-            <input type="text" id="phone" [(ngModel)]="data.phone" class="form-control" name="phone">
-        </form>
-    </div>
-</mat-dialog-content>
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-<mat-dialog-actions align="end">
-    <button mat-button mat-flat-button mat-dialog-close>Cancel</button>
-    <button mat-button mat-flat-button color="primary" [mat-dialog-close]="true">Add</button>
-</mat-dialog-actions>
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'customer', component: CustomerComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
