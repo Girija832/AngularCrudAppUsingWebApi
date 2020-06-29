e9d2585153b7462f894774a98742bd57e830e54f
unknown <debajyotimallick1993@gmail.com>
2020-06-29 11:02:46 +0530

    code refactor

--- src/app/shared/authconfig.interceptor.ts
+++ src/app/shared/authconfig.interceptor.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { AuthService } from '../services/auth.service';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-    ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if(this.authService.isLoggedIn() !== true) {
-      this.router.navigate(['login'])
+
+
+@Injectable()
+
+export class AuthInterceptor implements HttpInterceptor {
+    constructor(private authService: AuthService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if(this.authService.isLoggedIn()){
+        const authToken = this.authService.getAuthToken();
+        req = req.clone({
+            setHeaders: {
+                Authorization: 'Bearer ' + authToken
+            }
+        });
+
+        }
+        return next.handle(req);
     }
-    return true;
-  }
-
-  
 }
